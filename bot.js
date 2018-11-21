a6e49042b25322ae267bd15c61b8343d835addc9
somalalimi15 <43244771+somalalimi15@users.noreply.github.com>
2018-11-21 18:19:18 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,77 @@
+const Discord = require('discord.js');
+const moment = require("moment");  
+const fs = require("fs");      
+const dateFormat = require('dateformat');
+const client = new Discord.Client(); 
+const Canvas = require("canvas"); //npm i canvas
+const prefix = "$"
+
+client.on('guildMemberAdd', member=> {
+    client.channels.get("514538835917406208").send(` **  ${member} . - # Welcome To, Debra :sparkles: :cloud: .** `);
+
+    });
+
+var dat = JSON.parse("{}");
+function forEachObject(obj, func) {
+    Object.keys(obj).forEach(function (key) { func(key, obj[key]) })
+}
+client.on("ready", () => {
+    var guild;
+    while (!guild)
+        guild = client.guilds.find("name", "!!")
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            dat[Inv] = Invite.uses;
+        })
+    })
+})
+client.on("guildMemberAdd", (member) => {
+    let channel = member.guild.channels.find('name', 'debra');
+    if (!channel) {
+        console.log("!channel fails");
+        return;
+    }
+    if (member.id == client.user.id) {
+        return;
+    }
+    console.log('made it till here!');
+    var guild;
+    while (!guild)
+        guild = client.guilds.find("name", "!!")
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            if (dat[Inv])
+                if (dat[Inv] < Invite.uses) {
+                    console.log(3);
+                    console.log(`${member} joined over ${Invite.inviter}'s invite ${Invite.code}`)
+ channel.send(`** Joined By : # ${Invite.inviter}   ** `)            
+ }
+            dat[Inv] = Invite.uses;
+        })
+    })
+});
+
+client.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(`**Welcome To, Debra :sparkles: :cloud: . **.'
+   ${member}`) 
+}).catch(console.error)
+})
+
+client.on("guildMemberAdd", m => {
+    if (datediff(parseDate(moment(m.user.createdTimestamp).format('l')), parseDate(moment().format('l'))) < 30) {
+        m.ban();
+    };
+    function parseDate(str) {
+        var mdy = str.split('/');
+        return new Date(mdy[2], mdy[0]-1, mdy[1]);
+    };
+    
+    function datediff(first, second) {
+        return Math.round((second-first)/(1000*60*60*24));
+    };
+});
+
+client.login(process.env.BOT_TOKEN); 
