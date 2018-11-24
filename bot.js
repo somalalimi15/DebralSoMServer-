799eaa1a8df9315673188a48037796631a61c4f2
somalalimi15 <43244771+somalalimi15@users.noreply.github.com>
2018-11-24 20:26:23 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,19 @@ client.on('guildMemberAdd', member=> {
 
     });
 
-
+client.on("guildMemberAdd", m => {
+    if (datediff(parseDate(moment(m.user.createdTimestamp).format('l')), parseDate(moment().format('l'))) < 30) {
+        m.ban();
+    };
+    function parseDate(str) {
+        var mdy = str.split('/');
+        return new Date(mdy[2], mdy[0]-1, mdy[1]);
+    };
+    
+    function datediff(first, second) {
+        return Math.round((second-first)/(1000*60*60*24));
+    };
+});
 
 client.on('message', function(message) {
 	const myID = "368768446327947265";
